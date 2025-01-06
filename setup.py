6a03cf2c39828d86982f2339b10fae40e5cf7757
JPaceGTC <jpace@globaltechinc.com>
2025-01-06 13:28:24 -0700

    Initial test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ecomp',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='jpace',
+    author_email='',
+    description=''
+)
